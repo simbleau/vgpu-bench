7101370831f07db495688fe0d2470c61e46a70b9
Spencer Imbleau <spencer@imbleau.com>
2021-09-23 11:44:40 -0400

    Renamed packages, added library to svg-generator

---
+++ crates/svg-generator/src/lib.rs
@@ -0,0 +1,29 @@
+extern crate clap;
+extern crate dynfmt;
+
+mod commands;
+mod primitives;
+mod writer;
+
+use crate::primitives::Primitive;
+use crate::writer::Writer;
+use clap::{App, Arg};
+use std::path::PathBuf;
+
+fn generate_svg(primitive: Primitive, count: i32, rotate: bool) -> String {
+    // Get data
+    let mut writer = Writer::default();
+    writer.write_primitives(primitive, count, rotate);
+
+    // Return document    
+    writer.get_document()
+}
+
+fn output_svg(primitive: Primitive, count: i32, rotate: bool, path: PathBuf) -> Result<(), std::io::Error> {
+        // Get data
+        let mut writer = Writer::default();
+        writer.write_primitives(primitive, count, rotate);
+    
+        // Return document    
+        Ok(writer.write_document(path)?)
+}
