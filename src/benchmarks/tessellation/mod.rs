68ab8a37860452fd01ca937130c1b5a3595b0691
Spencer Imbleau <spencer@imbleau.com>
2021-11-08 06:41:17 -0500

    Paths will now be generated if they do not exist for benchmarks

--- src/benchmarks/tessellation/mod.rs
+++ src/benchmarks/tessellation/mod.rs
@@ -2,14 +2,23 @@ pub mod error;
 pub mod profile;
 
 use error::Result;
+use std::{
+    io::{Error, ErrorKind},
+    path::PathBuf,
+};
 
 pub fn profile_dir_with_output<P>(input_dir_path: P, output_path: P) -> Result<()>
 where
     P: Into<std::path::PathBuf>,
 {
+    // Make sure path can exist
+    let output_path: PathBuf = output_path.into();
+    let parent_dir = output_path.parent().expect("Path must have a parent");
+    std::fs::create_dir_all(parent_dir)?;
+
     // Options
     let profiler = profile::SVGProfiler::new()
-        .writer(csv::Writer::from_path(output_path.into())?)
+        .writer(csv::Writer::from_path(output_path)?)
         .assets(input_dir_path.into(), false)
         .backend(tessellation::backends::default());
     // Run
