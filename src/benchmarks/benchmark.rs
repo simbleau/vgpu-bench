d973e94483771e9d6c6abd62d83cf464d1b11495
Spencer Imbleau <spencer@imbleau.com>
2022-02-08 11:39:15 -0500

    Reduced code

--- src/benchmarks/benchmark.rs
+++ src/benchmarks/benchmark.rs
@@ -5,8 +5,7 @@ pub struct BenchmarkFn(Box<dyn FnOnce(&DriverOptions) -> Result<()>>);
 
 impl BenchmarkFn {
     pub fn call(self, options: &DriverOptions) -> Result<()> {
-        self.0(options)?;
-        Ok(())
+        Ok(self.0(options)?)
     }
 
     pub fn from<F>(function: F) -> Result<Self>
