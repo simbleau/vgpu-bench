240b1ac68e371e698b60a45e1f03007d88589e54
Spencer Imbleau <spencer@imbleau.com>
2022-02-10 12:06:45 -0500

    Added tracers

--- src/models/unit.rs
+++ src/models/unit.rs
@@ -9,6 +9,7 @@ use crate::log_assert;
 use super::benchmark_metadata::BenchmarkMetadata;
 use super::{monitor::Monitor, BenchmarkFn};
 use anyhow::{anyhow, Result};
+use log::{debug, trace};
 
 pub struct Unit {
     pub data: BenchmarkMetadata,
@@ -43,8 +44,20 @@ impl Unit {
         crossbeam::scope(|scope| {
             for mon in self.monitors.iter_mut() {
                 scope.spawn(|_| {
+                    trace!(
+                        "{mon_name}: waiting on lifecycle 'before' barrier",
+                        mon_name = mon.metadata().name
+                    );
                     barrier.wait();
+                    trace!(
+                        "{mon_name}: released from lifecycle 'before' barrier",
+                        mon_name = mon.metadata().name
+                    );
                     mon.before();
+                    trace!(
+                        "{mon_name}: began monitoring",
+                        mon_name = mon.metadata().name
+                    );
                 });
             }
         })
@@ -58,8 +71,10 @@ impl Unit {
         crossbeam::scope(|scope| {
             for mon in self.monitors.iter() {
                 scope.spawn(|_| {
+                    trace!("{mon_name}: waiting on execution barrier", mon_name = mon.metadata().name);
                     barrier.wait();
-                    // Spin-wait on completion of Benchmark
+                    trace!("{mon_name}: released from execution barrier", mon_name = mon.metadata().name);
+                    // Spinlock on completion of Benchmark
                     loop {
                         thread::sleep(mon.metadata().frequency.as_duration());
                         let measurement = mon.poll();
@@ -67,13 +82,17 @@ impl Unit {
                             break;
                         } else {
                             // TODO save measurement
-                            println!("Poll: {measurement:?}");
+                            debug!("Monitor {{{mon_name}}}: Poll {{{measurement:?}}}", mon_name = mon.metadata().name);
                         }
                     }
+                    trace!("{mon_name}: broke execution spinlock", mon_name = mon.metadata().name);
                 });
             }
+                    trace!("{bm_name}: waiting on execution barrier", bm_name = self.metadata().name);
             barrier.wait();
+            trace!("{bm_name}: released from execution barrier", bm_name = self.metadata().name);
             func.call(options).unwrap();
+            trace!("{bm_name}: finished execution", bm_name = self.metadata().name);
             complete.store(true, Ordering::Release);
         })
         .map_err(|thread_ex| anyhow!("Unit thread exception: {thread_ex:?}"))?;
@@ -84,8 +103,20 @@ impl Unit {
             for mon in self.monitors.iter_mut() {
                 scope.spawn(|_| {
                     let barrier = Arc::clone(&barrier);
+                    trace!(
+                        "{mon_name}: waiting on lifecycle 'after' barrier",
+                        mon_name = mon.metadata().name
+                    );
                     barrier.wait();
+                    trace!(
+                        "{mon_name}: released from lifecycle 'after' barrier",
+                        mon_name = mon.metadata().name
+                    );
                     mon.after();
+                    trace!(
+                        "{mon_name}: finished monitoring",
+                        mon_name = mon.metadata().name
+                    );
                 });
             }
         })
