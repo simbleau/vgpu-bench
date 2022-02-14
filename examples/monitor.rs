54fb65fe362d8c0d22cdfa37a0f610e57b861753
Spencer Imbleau <spencer@imbleau.com>
2022-02-14 00:02:34 -0500

    Moved test to example for monitoring

--- examples/monitor.rs
+++ examples/monitor.rs
@@ -1,1 +1,39 @@
-// TODO
+use log::LevelFilter;
+use simplelog::{ColorChoice, Config, TermLogger, TerminalMode};
+use vgpu_bench::driver::Driver;
+use vgpu_bench::models::{
+    BenchmarkFn, BenchmarkMetadata, MonitorFrequency, Unit,
+};
+use vgpu_bench::monitors::{CpuUtilizationMonitor, HeartbeatMonitor};
+
+use std::thread;
+use std::time::Duration;
+
+pub fn main() {
+    let benchmark_data = BenchmarkMetadata { name: "test" };
+    let benchmark_fn = BenchmarkFn::from(|_| {
+        thread::sleep(Duration::from_secs(5));
+        Ok(())
+    });
+
+    let mut unit = Unit::new(benchmark_data, benchmark_fn);
+    unit.monitors_mut().push(Box::new(HeartbeatMonitor::new(
+        "Mon1",
+        MonitorFrequency::Hertz(3),
+    )));
+    unit.monitors_mut()
+        .push(Box::new(CpuUtilizationMonitor::default()));
+
+    Driver::builder()
+        .logger(TermLogger::new(
+            LevelFilter::Trace,
+            Config::default(),
+            TerminalMode::Mixed,
+            ColorChoice::Auto,
+        ))
+        .add(unit)
+        .build()
+        .run();
+
+    // TODO display monitoring and results
+}
