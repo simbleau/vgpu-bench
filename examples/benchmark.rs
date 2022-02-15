40f098134d994e7726419805ea9da7ed7839798c
Spencer Imbleau <spencer@imbleau.com>
2022-02-15 12:09:03 -0500

    Added benchmark example

--- examples/benchmark.rs
+++ examples/benchmark.rs
@@ -1,1 +1,43 @@
-// TODO
+use std::rc::Rc;
+use std::sync::Mutex;
+
+use log::LevelFilter;
+use rand::Rng;
+use simplelog::{ColorChoice, Config, TermLogger, TerminalMode};
+use vgpu_bench::driver::Driver;
+use vgpu_bench::models::Unit;
+
+pub fn main() {
+    let items = Rc::new(Mutex::new(vec![]));
+
+    let items_ref = items.clone();
+    let mut generator = rand::thread_rng();
+    let insert_benchmark = Unit::from("Bm-Push", move |_| {
+        let mut items = items_ref.try_lock().unwrap();
+        // Benchmark Vec's push algorithm
+        for _ in 0..10_000_000 {
+            items.push(generator.gen::<u64>());
+        }
+        Ok(())
+    });
+
+    let items_ref = items.clone();
+    let sort_benchmark = Unit::from("Bm-Sort", move |_| {
+        let mut items = items_ref.try_lock().unwrap();
+        // Benchmark Vec's sorting algorithm
+        items.sort();
+        Ok(())
+    });
+
+    Driver::builder()
+        .logger(TermLogger::new(
+            LevelFilter::Info,
+            Config::default(),
+            TerminalMode::Mixed,
+            ColorChoice::Auto,
+        ))
+        .add(insert_benchmark)
+        .add(sort_benchmark)
+        .build()
+        .run();
+}
