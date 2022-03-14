029326b4115eec9a86036d1a3d66fd5eb24d9e97
Spencer Imbleau <spencer@imbleau.com>
2022-03-14 19:20:57 -0400

    Converted to proc_macro_attribute to derive inside a derive

--- crates/proc-macro-measurable/src/lib.rs
+++ crates/proc-macro-measurable/src/lib.rs
@@ -1,25 +1,18 @@
+#![feature(proc_macro_quote)]
+
 extern crate proc_macro;
+use proc_macro::*;
 
-use proc_macro::TokenStream;
-use quote::quote;
-use syn;
-
-#[proc_macro_derive(Measurable)]
-pub fn proc_macro_measurable(input: TokenStream) -> TokenStream {
-    // Construct a representation of code as a syntax tree that we can
-    // manipulate
-    let ast = syn::parse(input).unwrap();
-
-    // Build the trait implementation
-    impl_measurable_macro(&ast)
-}
-
-fn impl_measurable_macro(ast: &syn::DeriveInput) -> TokenStream {
-    let name = &ast.ident;
-    let expanded = quote! {
-        impl Send for #name {}
-        impl Sync for #name {}
-        [#derive(serde::Serialize, Debug)]
-    };
-    TokenStream::from(expanded)
+#[proc_macro_attribute]
+pub fn measurable_attribute(
+    _input: TokenStream,
+    annotated_item: TokenStream,
+) -> TokenStream {
+    quote! {
+        unsafe impl Sync for Measurable {{}}
+        unsafe impl Send for Measurable {{}}
+        #[derive(serde::Serialize, Debug)]
+        #annotated_item
+    }
+    .into()
 }
