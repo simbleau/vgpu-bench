7ded4cca7ceed574f1750cf4ca8d15cb5d66ccf5
Spencer Imbleau <spencer@imbleau.com>
2022-03-14 19:49:58 -0400

    Disabled macro for the time being to continue dev

--- crates/proc-macro-measurable/src/lib.rs
+++ crates/proc-macro-measurable/src/lib.rs
@@ -2,17 +2,26 @@
 
 extern crate proc_macro;
 use proc_macro::*;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_attribute]
 pub fn measurable_attribute(
     _input: TokenStream,
     annotated_item: TokenStream,
 ) -> TokenStream {
+    /*
+    let input = parse_macro_input!(annotated_item);
+    let DeriveInput { ident, .. } = input;
     quote! {
-        unsafe impl Sync for Measurable {{}}
-        unsafe impl Send for Measurable {{}}
+        unsafe impl Sync for $ident {}
+        unsafe impl Send for $ident {}
         #[derive(serde::Serialize, Debug)]
         $annotated_item
     }
     .into()
+    */
+    // TODO disabled for the time being.
+    quote! {
+        $annotated_item
+    }
 }
