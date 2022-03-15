8a87c42766b2200ed0a2bcb7dfe25bab528166e2
Spencer Imbleau <spencer@imbleau.com>
2022-03-15 10:54:19 -0400

    Qualified macro derives for hygiene

--- crates/proc-macro-measurable/src/lib.rs
+++ crates/proc-macro-measurable/src/lib.rs
@@ -15,9 +15,9 @@ pub fn measurement(
     let DeriveInput { ident, .. } = parse_macro_input!(clone);
     let name = ident.to_token_stream();
     quote! {
-        unsafe impl Sync for $name {}
-        unsafe impl Send for $name {}
-        #[derive(Debug)]
+        unsafe impl ::core::marker::Send for $name {}
+        unsafe impl ::core::marker::Sync for $name {}
+        #[derive(::core::fmt::Debug)]
         #[derive(::serde::Serialize)]
         $annotated_item
     }
