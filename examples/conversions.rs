36f4e39c2935c6464c8b0321684e4e86e22f8ea2
Spencer Imbleau <spencer@imbleau.com>
2022-03-23 07:03:39 -0400

    Simplified conversions example

--- examples/conversions.rs
+++ examples/conversions.rs
@@ -4,27 +4,27 @@ use vgpu_bench::{monitors::CpuUtilizationMonitor, prelude::*};
 
 #[measurement]
 struct RenderTime {
-    render_time_ms: u64,
+    render_time_ms: f32,
 }
 
 pub fn main() -> Result<()> {
     // Init logging
     vgpu_bench::util::logging::init_default();
 
-    let func = || {
+    let closure = || {
         let mut measurements = Measurements::new();
         // Collect real measurements here...
-        for i in 0..50 {
-            let render_time_ms = (100.0 + 50.0 * (i as f32).sin()) as u64;
+        for i in 0..5 {
+            let render_time_ms = 1.0 + 0.5 * (i as f32).sin();
             measurements.push(RenderTime { render_time_ms });
-            thread::sleep(Duration::from_millis(render_time_ms));
+            thread::sleep(Duration::from_secs_f32(render_time_ms));
         }
         // Benchmarking done!
         Ok(measurements)
     };
 
     // Convert closure into GPU-annotated `BenchmarkFn`
-    let benchmk_fn: BenchmarkFn<RenderTime> = func.into();
+    let benchmk_fn: BenchmarkFn<RenderTime> = closure.into();
     // Create `Benchmark` from `BenchmarkFn`
     let benchmark: Benchmark<RenderTime> =
         Benchmark::from(benchmk_fn).monitor(CpuUtilizationMonitor {
